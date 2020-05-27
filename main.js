ee92ff772540af54c670f1357e3fe568f24113f1
Tabeed Hameed <tabeedhameed121@gmail.com>
2020-05-27 18:54:29 +0530

    added files

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 window.addEventListener('keypress', function(pressedKey){
     let selectSound = document.querySelector(`audio[data-key = "${pressedKey.keyCode}"]`)
+    //console.log(selectSound)
     selectSound.currentTime = 0;
     selectSound.play()
 })
